353df05a60bdba604ed2dd003817822ec08dc1ce
wandering_nail <1754323999@qq.com>
2022-10-02 20:20:20 +0800

    new file:   apb/rtl/apb_rx.v 	new file:   apb/rtl/apb_tx.v 	new file:   apb/sim/tb_apb.v

---
+++ apb/rtl/apb_rx.v
@@ -0,0 +1,61 @@
+module apb_rx #(
+    parameter DATA_BW = 8,
+    parameter ADDR_BW = 8
+)(
+    input                  clk,
+    input                  rst_n,
+    input                  psel,
+    input                  penable,
+    input                  pwrite,
+    input  [ADDR_BW-1 : 0] paddr,
+    input  [DATA_BW-1 : 0] pwdata,
+    output [DATA_BW-1 : 0] prdata,
+    output                 pready
+);
+
+reg [DATA_BW-1 : 0] mem [(1 << ADDR_BW)-1 : 0];
+
+reg [1 : 0] cur_state;
+reg [1 : 0] next_state;
+
+localparam IDLE = 2'b00,
+           SEL  = 2'b01,
+           ACCE = 2'b10;
+
+assign pready = 1;
+    
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        cur_state <= IDLE;
+    end
+    else begin
+        cur_state <= next_state;
+    end
+end
+
+always @(*) begin
+    next_state = IDLE;
+    case (cur_state)
+        IDLE: next_state = psel ? SEL : IDLE;
+        SEL:  next_state = ACCE;
+        ACCE: begin
+            if (pready) begin
+                next_state = psel ? SEL : IDLE;
+            end
+            else begin
+                next_state = ACCE;
+            end
+        end
+    endcase
+end
+
+
+always @(posedge clk or negedge rst_n) begin
+    if (pwrite && pready) begin
+        mem[paddr] <= pwdata;
+    end
+end
+
+assign prdata = mem[paddr];
+
+endmodule
---
+++ apb/sim/tb_apb.v
@@ -0,0 +1,106 @@
+module tb_apb;
+
+localparam DATA_BW = 4,
+           ADDR_BW = 4;
+               
+reg                        clk;
+reg                        rst_n;
+
+reg  [DATA_BW+ADDR_BW : 0] cmd_in;
+reg                        cmd_vld;
+wire [DATA_BW-1 : 0]       prdata;
+wire                       pready;
+wire                       cmd_rdy;
+
+wire                       psel;
+wire                       penable;
+wire                       pwrite;
+wire [ADDR_BW-1 : 0]       paddr;
+wire [DATA_BW-1 : 0]       pwdata;
+wire [DATA_BW-1 : 0]       read_data;
+wire                       read_vld;
+
+reg [4 : 0] cnt;
+    
+wire cmd_fire = cmd_rdy && cmd_vld;
+    
+initial begin
+    $dumpfile("apb.vcd");
+    $dumpvars;
+end
+
+initial begin
+    clk = 0;
+    forever begin
+        #5;
+        clk = ~clk;
+    end
+end
+
+initial begin
+    rst_n = 0;
+    #3;
+    rst_n = 1;
+    #1000;
+    $finish;
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        cnt <= 'b0;
+    end
+    else if (cmd_fire) begin
+        cnt <= cnt + 1'b1;
+    end
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin 
+        cmd_vld <= 'b0;
+        cmd_in  <= 'b0;
+    end
+    else begin
+        cmd_vld <= 1'b1;
+        cmd_in  <= {~cnt[4],cnt[3:0],cnt[3:0]};
+    end
+end
+    
+
+apb_tx #(
+    .DATA_BW(DATA_BW),
+    .ADDR_BW(ADDR_BW)
+) apb_tx(
+    .clk(clk),
+    .rst_n(rst_n),
+    .cmd_in(cmd_in),
+    .cmd_rdy(cmd_rdy),
+    .cmd_vld(cmd_vld),
+    .read_vld(read_vld),
+    .read_data(read_data),
+    .psel(psel),
+    .penable(penable),
+    .pwrite(pwrite),
+    .paddr(paddr),
+    .pwdata(pwdata),
+    .prdata(prdata),
+    .pready(pready)
+);
+
+apb_rx #(
+    .DATA_BW(DATA_BW),
+    .ADDR_BW(ADDR_BW)
+) apb_rx(
+    .clk(clk),
+    .rst_n(rst_n),
+    .psel(psel),
+    .penable(penable),
+    .pwrite(pwrite),
+    .paddr(paddr),
+    .pwdata(pwdata),
+    .prdata(prdata),
+    .pready(pready)
+);
+    
+endmodule
+
+
